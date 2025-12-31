1a2eace068792b36d623926ba4d107662f35a940
NexusZenLmao <nexzenomg@gmail.com>
2025-12-31 21:58:36 +0300

    Update exoua.lua

--- exoua.lua
+++ exoua.lua
@@ -4,56 +4,32 @@ local Level = {}
 Level.__index = Level
 
 function exoua.level(meta)
-  return setmetatable({
-    metadata = {
-      name = meta.name or "unnamed",
-      author = meta.author or "unknown",
-      version = meta.version or 1
-    },
-    objects = {}
-  }, Level)
+  local self = setmetatable({}, Level)
+  self.metadata = meta or {}
+  self.objects = {}
+  return self
 end
 
-function Level:add(object_type, data)
-  local obj = {
-    type = object_type
-  }
-
-  if data.position then
-    obj.x = data.position.x or 0
-    obj.y = data.position.y or 0
-  end
-
-  if data.size then
-    for k, v in pairs(data.size) do
+local function push(self, kind, props)
+  local obj = { type = kind }
+  if props then
+    for k, v in pairs(props) do
       obj[k] = v
     end
   end
-
-  if data.scale then
-    obj.scaleX = data.scale.x or 1
-    obj.scaleY = data.scale.y or 1
-  end
-
-  for k, v in pairs(data) do
-    if k ~= "position" and k ~= "size" and k ~= "scale" then
-      obj[k] = v
-    end
-  end
-
   table.insert(self.objects, obj)
 end
 
-function Level:terrain(data)
-  self:add("terrain", data)
+function Level:terrain(props)
+  push(self, "terrain", props)
 end
 
-function Level:killer(data)
-  self:add("killer", data)
+function Level:killer(props)
+  push(self, "killer", props)
 end
 
-function Level:object(object_type, data)
-  self:add(object_type, data)
+function Level:object(kind, props)
+  push(self, kind, props)
 end
 
 function Level:export()
