cb793c9f85e8ca0f51e843ab8ffd87ccdb0fa02f
NexusZenLmao <nexzenomg@gmail.com>
2025-12-31 20:21:44 +0300

    Implement object creation functions in objects.lua
    Added functions to create platform, spike, and player objects.

--- lua/objects.lua
+++ lua/objects.lua
@@ -1,1 +1,31 @@
+local api = require("lua.api")
 
+local objects = {}
+
+function objects.platform(x, y, w, h)
+  api.add({
+    kind = "platform",
+    x = x,
+    y = y,
+    w = w,
+    h = h
+  })
+end
+
+function objects.spike(x, y)
+  api.add({
+    kind = "spike",
+    x = x,
+    y = y
+  })
+end
+
+function objects.player(x, y)
+  api.add({
+    kind = "player",
+    x = x,
+    y = y
+  })
+end
+
+return objects
