d48cc14b70cea0137be34e6f2b5a67a62dee7dce
NexusZenLmao <nexzenomg@gmail.com>
2026-01-01 02:53:49 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,27 @@
 local exoua = require("exoua")
 
+-- Create a new level with metadata
 local lvl = exoua.level{
   name = "Test Level",
   author = "Nexus",
   version = 1
 }
 
+-- Add terrain object
 lvl:terrain{
   pos = { x = 0, y = 0 }
 }
 
+-- Add killer object
 lvl:killer{
   pos = { x = 3, y = 1 }
 }
 
-return lvl:export()
+-- Export the level data
+local level_data = lvl:export()
+
+-- Print as JSON for Rust to consume
+local json = require("json")
+print(json.encode(level_data))
+
+return level_data
