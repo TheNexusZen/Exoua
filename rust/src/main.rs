f50842eaea84d9a0b0c54bb8f53459eb7dae51f8
NexusZenLmao <nexzenomg@gmail.com>
2026-01-01 01:51:56 +0300

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,12 +7,16 @@ use exolvl::{Read as ExoRead, Write as ExoWrite};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let compressed = fs::read("sample.exolvl")?;
 
-    let mut cursor = Cursor::new(compressed);
+    let raw = exolvl::gzip::decompress_bytes(&compressed)?;
+
+    let mut cursor = Cursor::new(raw);
     let exo = Exolvl::read(&mut cursor)?;
 
-    let mut out = Vec::new();
-    exo.write(&mut out)?;
+    let mut out_raw = Vec::new();
+    exo.write(&mut out_raw)?;
 
-    fs::write("out.exolvl", out)?;
+    let out_compressed = exolvl::gzip::compress(&out_raw)?;
+    fs::write("out.exolvl", out_compressed)?;
+
     Ok(())
 }
