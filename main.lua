e4ed52e3833359324b1af298345b43d2aab5de48
NexusZenLmao <nexzenomg@gmail.com>
2025-12-31 21:07:55 +0300

    Refactor main.lua to define level structure
    Removed physics simulation and print statements, added level metadata and objects.

--- main.lua
+++ main.lua
@@ -1,21 +1,13 @@
-print("Exoua booting")
+level = {
+  metadata = {
+    name = "test",
+    author = "exoua",
+    version = 1
+  },
+  objects = {
+    { type = "platform", x = 0, y = 0, w = 5, h = 1 },
+    { type = "spike", x = 3, y = 1 }
+  }
+}
 
-local physics = {}
-
-function physics.step(pos, vel, dt)
-    return {
-        x = pos.x + vel.x * dt,
-        y = pos.y + vel.y * dt
-    }
-end
-
-local position = { x = 0, y = 0 }
-local velocity = { x = 5, y = 0 }
-local dt = 1 / 60
-
-for i = 1, 60 do
-    position = physics.step(position, velocity, dt)
-end
-
-print("Final position:")
-print(position.x, position.y)
+return level
