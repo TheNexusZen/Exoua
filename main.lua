1f790c0a98ef9b09b910c83b6572d44f3abd9dc0
NexusZenLmao <nexzenomg@gmail.com>
2026-01-01 01:08:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,24 @@
 local exoua = require("exoua")
 
-local lvl = exoua.level{
-  name = "test",
-  author = "me",
-  version = 1
+local lvl = {
+  metadata = {
+    name = "Test Level",
+    author = "Exoua",
+    version = 1
+  },
+
+  objects = {
+    {
+      type = "terrain",
+      pos = { x = 0, y = 0 },
+      size = { w = 5, h = 1 },
+      color = "FFFFFFFF"
+    },
+    {
+      type = "killer",
+      pos = { x = 3, y = 1 }
+    }
+  }
 }
 
-lvl:terrain{
-  pos = { x = 0, y = 0 }
-}
-
-lvl:killer{
-  pos = { x = 3, y = 1 }
-}
-
-return lvl:export()
+return lvl
