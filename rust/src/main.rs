9027ddc887737c0988c374ef52bc31e8b1a261f9
NexusZenLmao <nexzenomg@gmail.com>
2026-01-01 01:23:36 +0300

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,18 @@
 use std::fs;
+use std::io::Write;
+
 use exolvl::types::exolvl::Exolvl;
-use exolvl::Write;
+use exolvl::{Read, Write as ExoWrite};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let data = fs::read("sample.exolvl")?;
+
     let exo = Exolvl::read(&data)?;
 
     let mut out = Vec::new();
     exo.write(&mut out)?;
 
     fs::write("out.exolvl", out)?;
+
     Ok(())
 }
