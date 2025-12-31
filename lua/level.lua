a72e25a7e4fa866c07b790eea88944f35742ffdd
NexusZenLmao <nexzenomg@gmail.com>
2025-12-31 20:30:45 +0300

    Update level.lua

--- lua/level.lua
+++ lua/level.lua
@@ -1,1 +1,11 @@
+local objects = require("lua.objects")
+local api = require("lua.api")
 
+objects.player(0, 0)
+
+objects.platform(0, -2, 6, 0.5)
+objects.platform(8, -1, 4, 0.5)
+
+objects.spike(4, -1.5)
+
+return api.export()
