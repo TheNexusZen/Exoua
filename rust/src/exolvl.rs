726cd0cf50d0bdd81fd67709d5c96d2ed02bd0a2
NexusZenLmao <nexzenomg@gmail.com>
2026-01-01 16:46:11 +0300

    Update exolvl.rs

--- rust/src/exolvl.rs
+++ rust/src/exolvl.rs
@@ -1,43 +1,29 @@
 use serde::{Deserialize, Serialize};
 use std::io::{self, Write};
+use uuid::Uuid;
 
-/// Exoracer object types
-#[repr(u8)]
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub enum ObjectType {
-    Terrain = 0,
-    Platform = 1,
-    Killer = 2,
-    Checkpoint = 3,
-    Start = 4,
-    Finish = 5,
-    MovingPlatform = 6,
-    Boost = 7,
-    Teleporter = 8,
-}
+/// Exoracer level format
+/// Magic: "NYA^" (0x4E59415E)
+/// Version: 19
 
-impl ObjectType {
-    pub fn from_string(s: &str) -> Option<Self> {
-        match s.to_lowercase().as_str() {
-            "terrain" => Some(ObjectType::Terrain),
-            "platform" => Some(ObjectType::Platform),
-            "killer" | "spike" | "hazard" => Some(ObjectType::Killer),
-            "checkpoint" => Some(ObjectType::Checkpoint),
-            "start" => Some(ObjectType::Start),
-            "finish" => Some(ObjectType::Finish),
-            "movingplatform" | "moving_platform" => Some(ObjectType::MovingPlatform),
-            "boost" => Some(ObjectType::Boost),
-            "teleporter" => Some(ObjectType::Teleporter),
-            _ => None,
-        }
-    }
-    
-    pub fn to_u8(self) -> u8 {
-        self as u8
-    }
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Level {
+    pub uuid: String,
+    pub name: String,
+    pub author: String,
+    pub theme: String,
+    pub objects: Vec<LevelObject>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LevelObject {
+    pub obj_type: u32,
+    pub position: Vector3,
+    pub rotation: Vector3,
+    pub scale: Vector3,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Vector3 {
     pub x: f32,
     pub y: f32,
@@ -49,138 +35,22 @@ impl Vector3 {
         Self { x, y, z }
     }
     
-    pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        writer.write_all(&self.x.to_le_bytes())?;
-        writer.write_all(&self.y.to_le_bytes())?;
-        writer.write_all(&self.z.to_le_bytes())?;
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-    pub a: u8,
-}
-
-impl Color {
-    pub fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
-        Self { r, g, b, a }
+    pub fn zero() -> Self {
+        Self::new(0.0, 0.0, 0.0)
     }
     
-    pub fn white() -> Self {
-        Self::new(255, 255, 255, 255)
+    pub fn one() -> Self {
+        Self::new(1.0, 1.0, 1.0)
     }
-    
-    pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        writer.write_all(&[self.r, self.g, self.b, self.a])?;
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LevelObject {
-    pub object_type: ObjectType,
-    pub position: Vector3,
-    pub size: Vector3,
-    pub rotation: Vector3,
-    pub color: Color,
-}
-
-impl LevelObject {
-    pub fn new(object_type: ObjectType, position: Vector3) -> Self {
-        Self {
-            object_type,
-            position,
-            size: Vector3::new(1.0, 1.0, 1.0),
-            rotation: Vector3::new(0.0, 0.0, 0.0),
-            color: Color::white(),
-        }
-    }
-    
-    pub fn with_size(mut self, size: Vector3) -> Self {
-        self.size = size;
-        self
-    }
-    
-    pub fn with_color(mut self, color: Color) -> Self {
-        self.color = color;
-        self
-    }
-    
-    pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        // Write object type as u8
-        writer.write_all(&[self.object_type.to_u8()])?;
-        
-        // Write position
-        self.position.write(writer)?;
-        
-        // Write size
-        self.size.write(writer)?;
-        
-        // Write rotation
-        self.rotation.write(writer)?;
-        
-        // Write color
-        self.color.write(writer)?;
-        
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LevelMetadata {
-    pub name: String,
-    pub author: String,
-    pub version: u32,
-    pub description: String,
-}
-
-impl LevelMetadata {
-    pub fn new(name: String, author: String) -> Self {
-        Self {
-            name,
-            author,
-            version: 1,
-            description: String::new(),
-        }
-    }
-    
-    pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        // Write version as u32 (little-endian)
-        writer.write_all(&self.version.to_le_bytes())?;
-        
-        // Write name length and name
-        let name_bytes = self.name.as_bytes();
-        writer.write_all(&(name_bytes.len() as u32).to_le_bytes())?;
-        writer.write_all(name_bytes)?;
-        
-        // Write author length and author
-        let author_bytes = self.author.as_bytes();
-        writer.write_all(&(author_bytes.len() as u32).to_le_bytes())?;
-        writer.write_all(author_bytes)?;
-        
-        // Write description length and description
-        let desc_bytes = self.description.as_bytes();
-        writer.write_all(&(desc_bytes.len() as u32).to_le_bytes())?;
-        writer.write_all(desc_bytes)?;
-        
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Level {
-    pub metadata: LevelMetadata,
-    pub objects: Vec<LevelObject>,
 }
 
 impl Level {
-    pub fn new(metadata: LevelMetadata) -> Self {
+    pub fn new(name: String, author: String) -> Self {
         Self {
-            metadata,
+            uuid: Uuid::new_v4().to_string(),
+            name,
+            author,
+            theme: "mountains".to_string(),
             objects: Vec::new(),
         }
     }
@@ -190,22 +60,134 @@ impl Level {
     }
     
     pub fn write<W: Write>(&self, writer: &mut W) -> io::Result<()> {
-        // Write magic number/header (0x45584F4C = "EXOL" in ASCII)
-        writer.write_all(&0x45584F4C_u32.to_le_bytes())?;
+        // Magic number: "NYA^"
+        writer.write_all(&[0x4E, 0x59, 0x41, 0x5E])?;
         
-        // Write format version
-        writer.write_all(&1_u32.to_le_bytes())?;
+        // Version: 19
+        writer.write_all(&19u32.to_le_bytes())?;
         
-        // Write metadata
-        self.metadata.write(writer)?;
+        // UUID (36 bytes as string with $prefix)
+        let uuid_str = format!("${}", self.uuid);
+        writer.write_all(uuid_str.as_bytes())?;
         
-        // Write object count
+        // Unknown byte (seems to be object count or flag)
         writer.write_all(&(self.objects.len() as u32).to_le_bytes())?;
         
-        // Write all objects
-        for object in &self.objects {
-            object.write(writer)?;
+        // Name length (as single byte) + name
+        writer.write_all(&[self.name.len() as u8])?;
+        writer.write_all(self.name.as_bytes())?;
+        
+        // Padding/unknown data (observed pattern from sample)
+        // This appears to be author info or metadata
+        let author_bytes = self.author.as_bytes();
+        writer.write_all(&[0x00])?; // separator
+        writer.write_all(&[0xF2, 0xE1, 0xE2, 0x73, 0xB4, 0x48, 0xDE, 0x48])?;
+        writer.write_all(&[0x84, 0xC4, 0xB8, 0xAF, 0xB5, 0x48, 0xDE, 0x48])?;
+        
+        // Padding to align
+        for _ in 0..64 {
+            writer.write_all(&[0x00])?;
         }
+        
+        // Object count again
+        writer.write_all(&(self.objects.len() as u32).to_le_bytes())?;
+        
+        // Unknown section (appears in all files)
+        writer.write_all(&[0x00, 0x01])?; // flags?
+        
+        // UUID again
+        writer.write_all(uuid_str.as_bytes())?;
+        
+        // More metadata
+        writer.write_all(&1u32.to_le_bytes())?; // version?
+        writer.write_all(&1u32.to_le_bytes())?; // flag?
+        
+        // Padding
+        for _ in 0..48 {
+            writer.write_all(&[0x00])?;
+        }
+        
+        // Another count/flag section
+        writer.write_all(&2u32.to_le_bytes())?;
+        writer.write_all(&1u32.to_le_bytes())?;
+        writer.write_all(&[0x00, 0x01])?;
+        
+        // More padding
+        for _ in 0..26 {
+            writer.write_all(&[0x00])?;
+        }
+        
+        // Object type marker
+        writer.write_all(&2u32.to_le_bytes())?;
+        writer.write_all(&[0x00, 0x00, 0x00])?;
+        
+        // More padding (total 128 bytes of zeros observed)
+        for _ in 0..128 {
+            writer.write_all(&[0x00])?;
+        }
+        
+        // Object count one more time
+        writer.write_all(&1u32.to_le_bytes())?;
+        
+        // More zeros
+        for _ in 0..20 {
+            writer.write_all(&[0x00])?;
+        }
+        
+        // Theme name length + theme
+        writer.write_all(&[self.theme.len() as u8])?;
+        writer.write_all(self.theme.as_bytes())?;
+        
+        // Padding after theme
+        for _ in 0..15 {
+            writer.write_all(&[0x00])?;
+        }
+        
+        // Float values (appear to be default settings)
+        // 1.0 in IEEE 754 = 0x3F800000
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x13C
+        writer.write_all(&0.0f32.to_le_bytes())?; 
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x148
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x14C
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x158
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x15C
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x160
+        writer.write_all(&1.0f32.to_le_bytes())?; // 0x164
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        
+        // Color values (0.833... = 0x3F55D5D6)
+        let color_val = 0.8333333f32;
+        writer.write_all(&color_val.to_le_bytes())?; // 0x180
+        writer.write_all(&color_val.to_le_bytes())?;
+        writer.write_all(&color_val.to_le_bytes())?;
+        writer.write_all(&1.0f32.to_le_bytes())?; // alpha
+        
+        // More values (0.3 = 0x3E99999A)
+        writer.write_all(&0.3f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&0.0f32.to_le_bytes())?;
+        writer.write_all(&1.0f32.to_le_bytes())?;
+        
+        // Final flag
+        writer.write_all(&1u32.to_le_bytes())?;
+        
+        // Padding
+        for _ in 0..12 {
+            writer.write_all(&[0x00])?;
+        }
+        
+        // End marker? (observed: 0x96C20000 = -97.5 as float or just bytes)
+        writer.write_all(&[0x96, 0xC2, 0x00, 0x00, 0x00])?;
+        
+        // TODO: Write actual objects
+        // For now, this creates a valid empty level
         
         Ok(())
     }
@@ -215,4 +197,4 @@ impl Level {
         self.write(&mut buffer)?;
         Ok(buffer)
     }
-  }
+}
