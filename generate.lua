68f14d7277a64f20bd47b3622b24aa2570a7718c
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 22:05:49 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -1,18 +1,17 @@
 package.path = "./src/?.lua;./src/?/init.lua;" .. package.path
 
-local Writer   = require("exoua.writer")
-local Sections = require("exoua.sections")
+local Writer = require("exoua.writer")
 
 local out = assert(io.open("out.exolvl", "wb"))
 
 Writer.write(out, {
-    Sections.Metadata({}),
-    Sections.Theme({}),
-    Sections.Layers({}),
-    Sections.Objects({}),
-    Sections.Patterns({}),
-    Sections.Prefabs({}),
-    Sections.Scripts({}),
+    { id = "metadata", data = {} },
+    { id = "theme", data = {} },
+    { id = "layers", data = {} },
+    { id = "objects", data = {} },
+    { id = "patterns", data = {} },
+    { id = "prefabs", data = {} },
+    { id = "scripts", data = {} },
 })
 
 out:close()
