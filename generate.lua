01618ae357e23a7450d8b34e3018b90bc68969aa
NexusZenLmao <nexzenomg@gmail.com>
2026-01-06 00:19:58 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -15,8 +15,37 @@ end
 local level = {
     uuid = uuid16(),
     name = "Generated Level",
+    description = "",
     metadata = {
         created_time = os.time(),
         modified_time = os.time(),
     },
     layers = {
+        {
+            layer_id = 0,
+            name = "Main",
+            visible = true,
+            locked = false,
+            objects = {
+                {
+                    object_id = 0,
+                    prefab_id = 0,
+                    position = { x = 0, y = 0 },
+                    rotation = 0,
+                    scale = { x = 1, y = 1 },
+                    color = { r = 255, g = 255, b = 255, a = 255 },
+                }
+            }
+        }
+    },
+    novascript = {
+        variables = {},
+        actions = {},
+    }
+}
+
+local f = assert(io.open("test.exolvl", "wb"))
+Types.level.write(f, level)
+f:close()
+
+print("test.exolvl generated")
