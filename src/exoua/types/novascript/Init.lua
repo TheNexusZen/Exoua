a168d6d9581acf497ef8d3bcb1ce3b8ed8f8a994
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 21:27:24 +0300

    Update Init.lua

--- src/exoua/types/novascript/Init.lua
+++ src/exoua/types/novascript/Init.lua
@@ -1,1 +1,9 @@
-
+return {
+    action = require("exoua.types.novascript.action"),
+    action_type = require("exoua.types.novascript.action_type"),
+    activator = require("exoua.types.novascript.activator"),
+    nova_value = require("exoua.types.novascript.nova_value"),
+    parameter = require("exoua.types.novascript.parameter"),
+    static_type = require("exoua.types.novascript.static_type"),
+    variable = require("exoua.types.novascript.variable"),
+}
