ce168d5641c0e92dd44be3808ea9526b1413394a
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 21:40:43 +0300

    Update init.lua

--- src/exoua/init.lua
+++ src/exoua/init.lua
@@ -1,1 +1,7 @@
-
+return {
+    binary = require("exoua.binary"),
+    reader = require("exoua.reader"),
+    writer = require("exoua.writer"),
+    sections = require("exoua.sections"),
+    types = require("exoua.types")
+}
