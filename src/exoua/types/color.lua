15f7aca4321ef952f48725c942f40536404528f1
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 20:21:34 +0300

    Update color.lua

--- src/exoua/types/color.lua
+++ src/exoua/types/color.lua
@@ -1,1 +1,21 @@
+local Color = {}
+Color.__index = Color
 
+function Color.new(r, g, b, a)
+    return setmetatable({
+        r = r or 0.0,
+        g = g or 0.0,
+        b = b or 0.0,
+        a = a or 0.0,
+    }, Color)
+end
+
+function Color.from_rgba(r, g, b, a)
+    return Color.new(r, g, b, a)
+end
+
+function Color:to_rgba()
+    return self.r, self.g, self.b, self.a
+end
+
+return Color
