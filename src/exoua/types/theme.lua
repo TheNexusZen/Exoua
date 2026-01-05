3a0e68eed38f4316b1e604747fa0a5a0c39b1b6e
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 20:43:26 +0300

    Update theme.lua

--- src/exoua/types/theme.lua
+++ src/exoua/types/theme.lua
@@ -1,1 +1,32 @@
+local Theme = {}
+Theme.__index = Theme
 
+local Read = require("exoua.read")
+local Write = require("exoua.write")
+
+Theme.Mountains = "mountains"
+Theme.Halloween = "halloween"
+Theme.Christmas = "christmas"
+Theme.Custom = "custom"
+
+function Theme.default()
+    return Theme.Mountains
+end
+
+function Theme.read(reader)
+    local value = Read.string(reader)
+    if value == Theme.Mountains
+        or value == Theme.Halloween
+        or value == Theme.Christmas
+        or value == Theme.Custom
+    then
+        return value
+    end
+    error("InvalidTheme: " .. tostring(value))
+end
+
+function Theme.write(writer, value)
+    Write.string(writer, value)
+end
+
+return Theme
