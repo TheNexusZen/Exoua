9ced55ae3cfcadffd8bddd0cc30a2fe4417b37c3
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 22:04:29 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -1,43 +1,18 @@
-local Writer   = require("src.exoua.writer")
-local Sections = require("src.exoua.sections")
-local Types    = require("src.exoua.types")
+package.path = "./src/?.lua;./src/?/init.lua;" .. package.path
 
-local output = assert(io.open("out.exolvl", "wb"))
+local Writer   = require("exoua.writer")
+local Sections = require("exoua.sections")
 
-local level = {
-    metadata = {
-        title = "Generated Level",
-        description = "",
-        author = "Exoua",
-        version = 1,
-    },
+local out = assert(io.open("out.exolvl", "wb"))
 
-    theme = {
-        id = 0,
-    },
-
-    layers = {},
-
-    objects = {},
-
-    patterns = {},
-
-    prefabs = {},
-
-    scripts = {},
-
-    author_replay = nil,
-}
-
-Writer.write(output, {
-    Sections.Metadata(level.metadata),
-    Sections.Theme(level.theme),
-    Sections.Layers(level.layers),
-    Sections.Objects(level.objects),
-    Sections.Patterns(level.patterns),
-    Sections.Prefabs(level.prefabs),
-    Sections.Scripts(level.scripts),
-    Sections.AuthorReplay(level.author_replay),
+Writer.write(out, {
+    Sections.Metadata({}),
+    Sections.Theme({}),
+    Sections.Layers({}),
+    Sections.Objects({}),
+    Sections.Patterns({}),
+    Sections.Prefabs({}),
+    Sections.Scripts({}),
 })
 
-output:close()
+out:close()
