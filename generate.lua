f66d275f5f7fcd104f3f6f4b17bab683420a96ab
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 23:26:43 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -1,42 +1,33 @@
 package.path = "./src/?.lua;./src/?/init.lua;" .. package.path
 
-local binary  = require("exoua.binary")
-local writer  = require("exoua.writer")
+local binary   = require("exoua.binary")
+local writer   = require("exoua.writer")
 local sections = require("exoua.sections")
 
 local function write_section(f, id, fn)
     local buf = {}
     local mf = {
-        write = function(_, s) table.insert(buf, s) end
+        write = function(_, s) buf[#buf + 1] = s end
     }
 
     fn(mf)
 
     local data = table.concat(buf)
 
-    binary.write_u32(f, id)
-    binary.write_u32(f, #data)
+    binary.write_i32(f, id)
+    binary.write_i32(f, #data)
     f:write(data)
 end
 
 local f = assert(io.open("test.exolvl", "wb"))
 
--- =====================
--- LOCAL_LEVEL
--- =====================
 write_section(f, sections.LOCAL_LEVEL, function(w)
-
     writer.string(w, "00000000-0000-0000-0000-000000000001")
     writer.i32(w, os.time())
     writer.i32(w, os.time())
-
 end)
 
--- =====================
--- LEVEL
--- =====================
 write_section(f, sections.LEVEL, function(w)
-
     writer.i32(w, 1024)
     writer.i32(w, 1024)
 
@@ -45,24 +36,17 @@ write_section(f, sections.LEVEL, function(w)
 
     writer.i32(w, 0)
 
-    -- layers
     writer.list(w, {
         { id = 0, name = "Layer 1", visible = true, locked = false }
     }, function(layer)
-
         writer.i32(w, layer.id)
         writer.string(w, layer.name)
         writer.bool(w, layer.visible)
         writer.bool(w, layer.locked)
-
     end)
 
-    -- objects (must exist)
     writer.list(w, {}, function() end)
-
-    -- scripts
     writer.list(w, {}, function() end)
-
 end)
 
 f:close()
