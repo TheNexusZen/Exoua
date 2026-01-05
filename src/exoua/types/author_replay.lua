930a5699ea9f5b3980e8d9314c13dcbb416ac3ba
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 20:20:20 +0300

    Update author_replay.lua

--- src/exoua/types/author_replay.lua
+++ src/exoua/types/author_replay.lua
@@ -1,1 +1,18 @@
+local AuthorReplay = {}
+AuthorReplay.__index = AuthorReplay
 
+function AuthorReplay.new(bytes)
+    return setmetatable({
+        bytes = bytes or "",
+    }, AuthorReplay)
+end
+
+function AuthorReplay.from_bytes(bytes)
+    return AuthorReplay.new(bytes)
+end
+
+function AuthorReplay:to_bytes()
+    return self.bytes
+end
+
+return AuthorReplay
