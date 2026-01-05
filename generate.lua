161602207bce80105e78cd2c67fb47c151bdc847
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 23:39:49 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -1,55 +1,41 @@
+-- generate.lua
+
+-- make sure Lua can see ./src
 package.path = "./src/?.lua;./src/?/init.lua;" .. package.path
 
-local binary = require("exoua.binary")
-local writer = require("exoua.writer")
+local Level = require("exoua.types.level")
+local Writer = require("exoua.writer")
 
-local SECTION_LOCAL_LEVEL = 2
-local SECTION_LEVEL       = 3
+-- open output file
+local file = assert(io.open("test.exolvl", "wb"))
 
-local function write_section(f, id, fn)
-    local buf = {}
-    local mf = {
-        write = function(_, s) buf[#buf + 1] = s end
-    }
+-- construct a level object
+local level = Level.new({
+    version = 1,
 
-    fn(mf)
+    -- REQUIRED: must be a list, not nil
+    layers = {
+        {
+            id = 1,
+            name = "Main",
 
-    local data = table.concat(buf)
+            -- REQUIRED: must contain at least one object
+            objects = {
+                {
+                    id = 1,
 
-    binary.write_i32(f, id)
-    binary.write_i32(f, #data)
-    f:write(data)
-end
+                    position = { x = 0, y = 0 },
+                    rotation = 0,
+                    scale = { x = 1, y = 1 },
 
-local f = assert(io.open("test.exolvl", "wb"))
+                    -- minimal prefab
+                    prefab = {
+                        name = "test_object",
+                        properties = {}
+                    }
+                }
+            }
+        }
+    },
 
-write_section(f, SECTION_LOCAL_LEVEL, function(w)
-    writer.string(w, "00000000-0000-0000-0000-000000000001")
-    writer.i32(w, os.time())
-    writer.i32(w, os.time())
-end)
-
-write_section(f, SECTION_LEVEL, function(w)
-    writer.i32(w, 1024)
-    writer.i32(w, 1024)
-
-    writer.i32(w, 0)
-    writer.i32(w, 0)
-
-    writer.i32(w, 0)
-
-    writer.list(w, {
-        { id = 0, name = "Layer 1", visible = true, locked = false }
-    }, function(layer)
-        writer.i32(w, layer.id)
-        writer.string(w, layer.name)
-        writer.bool(w, layer.visible)
-        writer.bool(w, layer.locked)
-    end)
-
-    writer.list(w, {}, function() end)
-    writer.list(w, {}, function() end)
-end)
-
-f:close()
-print("Generated test.exolvl")
+    -- REQUIRED: empty list is
