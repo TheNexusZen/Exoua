e0c3941fb916b50ff9e7bd4eb6f604755d91325c
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 23:37:03 +0300

    Update writer.lua

--- src/exoua/writer.lua
+++ src/exoua/writer.lua
@@ -6,5 +6,7 @@ function writer.bool(f, v) binary.write_bool(f, v) end
 function writer.i32(f, v) binary.write_i32(f, v) end
 function writer.string(f, v) binary.write_string(f, v) end
 function writer.list(f, l, fn) binary.write_list(f, l, fn) end
+function writer.u32(f, v) binary.write_u32(f, v or 0) end
+function writer.f32(f, v) binary.write_f32(f, v or 0) end
 
 return writer
