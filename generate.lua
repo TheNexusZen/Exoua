6a72abe7f2639374b9c0bfef25db005970694311
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 23:41:10 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -1,25 +1,22 @@
 -- generate.lua
 
--- make sure Lua can see ./src
+-- allow Lua to find ./src
 package.path = "./src/?.lua;./src/?/init.lua;" .. package.path
 
 local Level = require("exoua.types.level")
-local Writer = require("exoua.writer")
 
 -- open output file
 local file = assert(io.open("test.exolvl", "wb"))
 
--- construct a level object
+-- create level
 local level = Level.new({
     version = 1,
 
-    -- REQUIRED: must be a list, not nil
     layers = {
         {
             id = 1,
             name = "Main",
 
-            -- REQUIRED: must contain at least one object
             objects = {
                 {
                     id = 1,
@@ -28,7 +25,6 @@ local level = Level.new({
                     rotation = 0,
                     scale = { x = 1, y = 1 },
 
-                    -- minimal prefab
                     prefab = {
                         name = "test_object",
                         properties = {}
@@ -38,4 +34,12 @@ local level = Level.new({
         }
     },
 
-    -- REQUIRED: empty list is
+    prefabs = {},
+    scripts = {},
+})
+
+-- write level
+level:write(file)
+file:close()
+
+print("Generated test.exolvl")
