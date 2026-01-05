e0f9a6544ee2c1814a0be41c1fc47060721c0e8b
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 20:26:55 +0300

    Update pattern.lua

--- src/exoua/types/pattern.lua
+++ src/exoua/types/pattern.lua
@@ -1,1 +1,19 @@
+local Pattern = {}
+Pattern.__index = Pattern
 
+function Pattern.new(pattern_id, pattern_frames)
+    return setmetatable({
+        pattern_id = pattern_id or 0,
+        pattern_frames = pattern_frames or {},
+    }, Pattern)
+end
+
+function Pattern.add_frame(self, image)
+    self.pattern_frames[#self.pattern_frames + 1] = image
+end
+
+function Pattern.get_frames(self)
+    return self.pattern_frames
+end
+
+return Pattern
