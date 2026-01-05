2ab816869ca081bdb874d438073144787862d143
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 21:37:48 +0300

    Update writer.lua

--- src/exoua/writer.lua
+++ src/exoua/writer.lua
@@ -1,1 +1,10 @@
+local binary = require("exoua.binary")
 
+local writer = {}
+
+function writer.bool(f, v) binary.write_bool(f, v) end
+function writer.i32(f, v) binary.write_i32(f, v) end
+function writer.string(f, v) binary.write_string(f, v) end
+function writer.list(f, l, fn) binary.write_list(f, l, fn) end
+
+return writer
