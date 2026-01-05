7d5f3a0fd5c5fadd5bef6f1cfb9b13fc131e853d
NexusZenLmao <nexzenomg@gmail.com>
2026-01-06 01:30:16 +0300

    Update generate.lua

--- generate.lua
+++ generate.lua
@@ -1,47 +1,58 @@
-package.path = "./src/?.lua;./src/?/init.lua;" .. package.path
+-- seed RNG (SAFE)
+math.randomseed(os.time())
 
-local Types = require("exoua.types")
-
-math.randomseed(os.time() + tonumber(tostring({}):sub(8), 16))
-
+-- UUID v4 generator
 local function uuid_v4()
     local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
     return template:gsub("[xy]", function(c)
-        local v = (c == "x") and math.random(0, 15)
-                  or math.random(8, 11)
+        local v
+        if c == "x" then
+            v = math.random(0, 15)
+        else
+            v = math.random(8, 11)
+        end
         return string.format("%x", v)
     end)
 end
 
-local level = {
-    uuid = uuid16(),
+-- requires
+local level_type = require("exoua.types.level")
 
+-- build level
+local level = {
+    uuid = uuid_v4(),
     name = "Generated Level",
     description = "",
-
-    metadata = {
-        created_time = os.time(),
-        modified_time = os.time(),
-    },
+    authorTime = 0,
 
     layers = {
         {
-            layer_id = 0,
-            name = "Main",
-            visible = true,
-            locked = false,
-            objects = {},
-        }
-    },
+            uuid = uuid_v4(),
+            name = "Main Layer",
+            objects = {
+                {
+                    uuid = uuid_v4(),
+                    name = "Test Object",
 
-    novascript = {
-        variables = {},
-        actions = {},
+                    position = { x = 0, y = 0 },
+                    rotation = 0,
+                    scale = { x = 1, y = 1 },
+
+                    prefab = {
+                        shape = "box",
+                        width = 1,
+                        height = 1,
+                    }
+                }
+            }
+        }
     }
 }
 
+-- write file
 local f = assert(io.open("test.exolvl", "wb"))
-Types.level.write(f, level)
+level_type.write(f, level)
 f:close()
 
-print("test.exolvl generated successfully")
+print("Generated test.exolvl")
+print("Level UUID:", level.uuid)
