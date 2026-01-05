75a7e3a9145d9cef81c4919fa0ddd9d8073c37b0
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 20:25:53 +0300

    Update enums.lua

--- src/exoua/types/enums.lua
+++ src/exoua/types/enums.lua
@@ -1,1 +1,32 @@
+local Enums = {}
 
+Enums.PhysicsType = {
+    None = 0,
+    Static = 1,
+    Dynamic = 2,
+    Kinematic = 3,
+}
+
+Enums.FillMode = {
+    Solid = 0,
+    Outline = 1,
+}
+
+Enums.Blending = {
+    Normal = 0,
+    Additive = 1,
+    Multiply = 2,
+}
+
+Enums.SimulationSpace = {
+    Local = 0,
+    World = 1,
+}
+
+Enums.EmitterShape = {
+    Point = 0,
+    Box = 1,
+    Circle = 2,
+}
+
+return Enums
