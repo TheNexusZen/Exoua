18a5456a2e97fe9f0d1d2c1ac52e8cef6c32a3d8
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 20:31:40 +0300

    Update vec2.lua

--- src/exoua/types/vec2.lua
+++ src/exoua/types/vec2.lua
@@ -1,1 +1,26 @@
+local Vec2 = {}
+Vec2.__index = Vec2
 
+local Read = require("exoua.read")
+local Write = require("exoua.write")
+
+function Vec2.new(x, y)
+    return setmetatable({
+        x = x or 0.0,
+        y = y or 0.0,
+    }, Vec2)
+end
+
+function Vec2.read(reader)
+    return Vec2.new(
+        Read.f32(reader),
+        Read.f32(reader)
+    )
+end
+
+function Vec2:write(writer)
+    Write.f32(writer, self.x)
+    Write.f32(writer, self.y)
+end
+
+return Vec2
