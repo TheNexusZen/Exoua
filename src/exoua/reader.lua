034c05ee91ef59f48ab2245afb30c38c5ea0a612
NexusZenLmao <nexzenomg@gmail.com>
2026-01-05 21:37:08 +0300

    Update reader.lua

--- src/exoua/reader.lua
+++ src/exoua/reader.lua
@@ -1,1 +1,10 @@
+local binary = require("exoua.binary")
 
+local reader = {}
+
+function reader.bool(f) return binary.read_bool(f) end
+function reader.i32(f) return binary.read_i32(f) end
+function reader.string(f) return binary.read_string(f) end
+function reader.list(f, fn) return binary.read_list(f, fn) end
+
+return reader
